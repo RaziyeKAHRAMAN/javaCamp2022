2bb8b62fbbe90c97a77f0356712b57f8038db3e3
ASUS <ASUS@SERVER.local>
2022-09-22 09:59:23 +0300

    14 eylül

---
+++ Sart/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String havadurumu="güneşli";
+		if (havadurumu=="Yagmurlu") {
+			System.out.println("şemsiyeni al");
+		}else if (havadurumu=="yagmur yagabilir") {
+			System.out.println("şemsiyeni alsan iyi olur");
+		}
+		else {
+			System.out.println("şemsiyeni alma");			
+		}
+		
+				
+		System.out.println("_______________");
+
+	}
+
+}
