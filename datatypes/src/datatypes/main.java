40b1821d4d1451849198b10e104c4d5b877b54e1
ASUS <ASUS@SERVER.local>
2022-09-22 09:56:12 +0300

    14 eylül

---
+++ datatypes/src/datatypes/main.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sayi =12;
+double sayi1 = 12.5 ;
+sayi = -126;
+
+	}
+
+}
