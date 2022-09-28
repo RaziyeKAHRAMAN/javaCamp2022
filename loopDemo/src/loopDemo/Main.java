c4c7056956cfcf8b55e9029cc8e08b70d9ef9041
ASUS <ASUS@SERVER.local>
2022-09-28 13:12:22 +0300

    methods

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,18 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("For Döngü Bitti");
+		int i = 1;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While Döngü Bitti");
+	}
+}
