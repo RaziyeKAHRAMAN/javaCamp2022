3391d1562e303c7023a3ea335fcb674afe0c6e64
ASUS <ASUS@SERVER.local>
2022-09-28 13:11:13 +0300

    else if

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int ogrenciSayisi=12;
+String mesaj="öğrenci Sayısı :" ;
+System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayısı : " + ogrenciSayisi );
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+	
+	}
+
+}
