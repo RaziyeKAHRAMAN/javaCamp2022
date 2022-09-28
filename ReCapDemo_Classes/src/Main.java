af4be5cd0b50485f714edf7cd54f0231d92a81a8
ASUS <ASUS@SERVER.local>
2022-09-28 13:13:49 +0300

    recap demo

---
+++ ReCapDemo_Classes/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dortİslem dortislem=new Dortİslem();
+		int sonuc=dortislem.Topla(3, 4);
+		System.out.print(sonuc);
+
+	}
+
+}
