ec41390907c8dbfa8f2a6c06b2ab2041590e6f23
ASUS <ASUS@SERVER.local>
2022-09-28 13:14:45 +0300

    switch

---
+++ switch/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char grade = 'E';
+		if(grade=='A') {
+			System.out.println("Mükemmel : Geçtiniz");
+		}else if(grade=='B') {
+			System.out.println("Çok Güzel : Geçtiniz");
+		}else if(grade=='C') {
+			System.out.println("İyi : Geçtiniz");
+		}else if(grade=='D') {
+			System.out.println("Fena Değil : Geçtiniz");
+		}else if(grade=='E') {
+			System.out.println("Maalesef Kaldınız");
+		}
+		else  {System.out.println("Geçersiz not girdiniz");}
+		
+
+		
+
+	}
+}
